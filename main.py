7882cae56ebc809b51909516e0d73af94a118749
Mick <mick@pop-os.localdomain>
2022-12-27 21:01:19 +0300

    Язык изменен на английский 	изменено:      main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Сумма чисел от 1 до n
-var_1 = int(input("Введите число: "))
+# Sum from 1 to n
+var_1 = int(input("Enter desired number: "))
 agregator = []
 for i in range(1, var_1+1):
     print(i, sep=" ", end=" ")
